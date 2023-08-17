8cc52c1c0823f84f015754333141651c05ef4c50
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2023-08-17 23:03:47 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//SJSVBMNLOVE!!!!!!11!!!!!/
+/*$('.multiple-items').slick({
+    prevArrow:'<i class="fa-solid fa-chevron-left blog_slick_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right blog_slick_arrow"></i>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });*/
+$('.count-start').counterUp({ delay: 20,
+    time: 2000,
+})
+$('.banner_slider').slick({
+    prevArrow:'<i class="fa-solid fa-chevron-left banner_slick_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-chevron-right banner_slick_arrow"></i>',
+    dots:true,
+    dotsClass:'banner_slider_dots',
+    /*fade: true,
+    autoplay: true,
+    autoplaySpeed:1000,
+    speed:1000,*/
+ });
